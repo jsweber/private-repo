432d08d3ac1147f70bcbcad0a09d36f3e644eb2f
Jiaying Li <lijiaying@pjlab.org.cn>
2023-02-06 18:25:16 +0800

    Update app.py

--- app.py
+++ app.py
@@ -3,9 +3,10 @@ import gradio as gr
 
 
 def hello(i):
-    classifier = pipeline("sentiment-analysis")
-    a = classifier(i)
-    return a
+    # classifier = pipeline("sentiment-analysis")
+    # a = classifier(i)
+    # return a
+    return "Hello World!"
 
 iface = gr.Interface(fn=hello, inputs="text", outputs="text")
 iface.launch()
